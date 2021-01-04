753e707269518661f3bf6214ca4b6011023da04b
Yucked <yucked@pm.me>
2021-01-04 16:20:55 -0500

    DNET events

--- Victoria.DNET/LavaNode.cs
+++ Victoria.DNET/LavaNode.cs
@@ -1,5 +1,45 @@
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Victoria.Wrappers;
+
 namespace Victoria.DNET {
     public class LavaNode : AbstractLavaNode {
-        public LavaNode(NodeConfiguration nodeConfiguration) : base(nodeConfiguration) { }
+        public LavaNode(NodeConfiguration nodeConfiguration, DiscordSocketClient socketClient)
+            : this(nodeConfiguration, socketClient as BaseSocketClient) { }
+
+        public LavaNode(NodeConfiguration nodeConfiguration, DiscordShardedClient shardedClient)
+            : this(nodeConfiguration, shardedClient as BaseSocketClient) { }
+
+        public LavaNode(NodeConfiguration nodeConfiguration, BaseSocketClient baseClient)
+            : base(nodeConfiguration) {
+            DiscordClient = new DiscordClient {
+                UserId = baseClient.CurrentUser.Id,
+                Shards = baseClient switch {
+                    DiscordSocketClient                => 0,
+                    DiscordShardedClient shardedClient => shardedClient.Shards.Count,
+                    _                                  => 0
+                }
+            };
+
+            baseClient.VoiceServerUpdated += OnVoiceServerUpdated;
+            baseClient.UserVoiceStateUpdated += OnUserVoiceStateUpdated;
+        }
+
+        private Task OnVoiceServerUpdated(SocketVoiceServer voiceServer) {
+            return DiscordClient.OnVoiceServerUpdated.Invoke(new VoiceServer {
+                GuildId = voiceServer.Guild.Id,
+                Endpoint = voiceServer.Endpoint,
+                Token = voiceServer.Token
+            });
+        }
+
+        private Task OnUserVoiceStateUpdated(SocketUser user, SocketVoiceState oldState,
+                                             SocketVoiceState currentState) {
+            return DiscordClient.OnUserVoiceStateUpdated.Invoke(new VoiceState {
+                UserId = user.Id,
+                OldSessionId = oldState.VoiceSessionId,
+                CurrentSessionId = currentState.VoiceSessionId
+            });
+        }
     }
 }
