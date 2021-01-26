21346cfa79ac395c3bf0293f655d9188ef6ed17a
Yucked <yucked@pm.me>
2021-01-26 11:12:52 -0500

    God I had unit tests

--- tests/CastingTests.cs
+++ tests/CastingTests.cs
@@ -1,40 +1,13 @@
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Victoria.Interfaces;
-using Victoria.Responses.Search;
+using Victoria.Tests.Objects;
 
 namespace Victoria.Tests {
     [TestClass]
     public sealed class CastingTests {
-        private readonly ILavaNode _lavaNode
-            = new AbstractLavaNode(new NodeConfiguration());
-
-        public sealed class CustomTrack : AbstractLavaTrack {
-            public ulong Requester { get; set; }
-
-            public CustomTrack(ILavaTrack lavaTrack) : base(lavaTrack) { }
-        }
-
-        [DataTestMethod]
-        [DataRow("South Side Suicide")]
-        [DataRow("Drake Passionfruit")]
-        public async Task Is_Type_Of_ILavaTrack_Async(string query) {
-            var searchResponse = await _lavaNode.SearchAsync(SearchType.YouTube, query);
-            if (searchResponse.Status == SearchStatus.LoadFailed || searchResponse.Status == SearchStatus.NoMatches) {
-                Assert.Fail("Failed search response.");
-            }
-
-            var lavaTrack = searchResponse.Tracks.FirstOrDefault();
-            var customTrack = new CustomTrack(lavaTrack) {
-                Requester = ulong.MaxValue
-            };
-
-            Assert.IsInstanceOfType(customTrack, typeof(ILavaTrack));
-            Assert.IsInstanceOfType(customTrack, typeof(AbstractLavaTrack));
-            Assert.IsNotNull(customTrack);
-            Assert.IsNotNull(customTrack.Requester);
-            Assert.IsTrue(customTrack.Requester is ulong.MaxValue);
+        [TestMethod]
+        public void IsLavaTrack() {
+            Assert.IsInstanceOfType(new CustomTrack(default), typeof(ILavaTrack));
         }
     }
 }
--- tests/SearchTests.cs
+++ tests/SearchTests.cs
@@ -1,13 +1,16 @@
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Victoria.Interfaces;
 using Victoria.Responses.Search;
+using Victoria.Tests.Objects;
 
 namespace Victoria.Tests {
     [TestClass]
     public class SearchTests {
         private readonly ILavaNode _lavaNode
-            = new AbstractLavaNode(new NodeConfiguration());
+            = Global.Provider.GetRequiredService<ILavaNode>();
 
         [DataTestMethod]
         [DataRow("The Weeknd Often")]
@@ -16,14 +19,14 @@ namespace Victoria.Tests {
         [DataRow("No Role Modelz")]
         public async Task SearchYouTubeAsync(string query) {
             var searchResponse = await _lavaNode.SearchAsync(SearchType.YouTube, query);
-            
+
             Assert.IsNotNull(searchResponse);
             Assert.IsNotNull(searchResponse.Exception, "searchResponse.Exception != null");
             Assert.IsNotNull(searchResponse.Playlist, "searchResponse.SearchPlaylist != null");
             Assert.IsNotNull(searchResponse.Tracks, "searchResponse.Tracks != null");
             Assert.IsNotNull(searchResponse.Status, "searchResponse.Status != null");
         }
-        
+
         [DataTestMethod]
         [DataRow("https://www.youtube.com/watch?v=Rnd1itAw-Iw")]
         [DataRow("https://www.youtube.com/watch?v=7OtHA_Hvho4")]
@@ -33,12 +36,35 @@ namespace Victoria.Tests {
         [DataRow("https://www.youtube.com/watch?v=JH398xAYpZA&list=OLAK5uy_lwaD8UXRautA8W9eWT4zZOvwf5Ktxpax8")]
         public async Task SearchDirectLinkAsync(string query) {
             var searchResponse = await _lavaNode.SearchAsync(SearchType.Direct, query);
-            
+
             Assert.IsNotNull(searchResponse);
             Assert.IsNotNull(searchResponse.Exception, "searchResponse.Exception != null");
             Assert.IsNotNull(searchResponse.Playlist, "searchResponse.SearchPlaylist != null");
             Assert.IsNotNull(searchResponse.Tracks, "searchResponse.Tracks != null");
             Assert.IsNotNull(searchResponse.Status, "searchResponse.Status != null");
+        }
+
+
+
+        [DataTestMethod]
+        [DataRow("South Side Suicide")]
+        [DataRow("Drake Passionfruit")]
+        public async Task IsTypeOfILavaTrack(string query) {
+            var searchResponse = await _lavaNode.SearchAsync(SearchType.YouTube, query);
+            if (searchResponse.Status == SearchStatus.LoadFailed || searchResponse.Status == SearchStatus.NoMatches) {
+                Assert.Fail("Failed search response.");
+            }
+
+            var lavaTrack = searchResponse.Tracks.FirstOrDefault();
+            var customTrack = new CustomTrack(lavaTrack) {
+                Requester = ulong.MaxValue
+            };
+
+            Assert.IsInstanceOfType(customTrack, typeof(ILavaTrack));
+            Assert.IsInstanceOfType(customTrack, typeof(AbstractLavaTrack));
+            Assert.IsNotNull(customTrack);
+            Assert.IsNotNull(customTrack.Requester);
+            Assert.IsTrue(customTrack.Requester is ulong.MaxValue);
         }
     }
 }
